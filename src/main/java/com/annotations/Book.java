ad8449b6b247a3d582267144b92b3ab77ffd55a4
TudorDan <tudor.ist@gmail.com>
2022-05-29 23:35:34 +0300

    Add mockito annotations

---
+++ src/main/java/com/annotations/Book.java
@@ -0,0 +1,37 @@
+package com.annotations;
+
+import java.time.LocalDate;
+
+public class Book {
+    private final String bookId;
+    private final String title;
+    private int price;
+    private final LocalDate publishDate;
+
+    public Book(String bookId, String title, int price, LocalDate publishDate) {
+        this.bookId = bookId;
+        this.title = title;
+        this.price = price;
+        this.publishDate = publishDate;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/annotations/BookService.java
@@ -0,0 +1,22 @@
+package com.annotations;
+
+import java.util.List;
+
+public class BookService {
+    private final BookDao bookDao;
+
+    public BookService(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    public List<Book> getNewBooksWithAppliedDiscount(int discountRate, int days) {
+        List<Book> newBooks = bookDao.findNewBooks(days);
+        // 500 apply 10% -> 10% of 500 -> 50 -> 500 - 50 -> 450
+        for (Book newBook : newBooks) {
+            int price = newBook.getPrice();
+            int newPrice = price - (discountRate * price / 100);
+            newBook.setPrice(newPrice);
+        }
+        return newBooks;
+    }
+}
